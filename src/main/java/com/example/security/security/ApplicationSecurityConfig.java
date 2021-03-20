63700a474115dd73fd54e79ae649d46d179daa1b
Conrad <conrad.lee@utexas.edu>
2021-03-20 17:39:38 -0500

    db-authentication, implementation of FakeApplicationUserDaoService and ApplicationUserService

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -1,19 +1,16 @@
 package com.example.security.security;
 
+import com.example.security.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
@@ -24,10 +21,12 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -51,7 +50,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/","index","/css/*","/js/*")  //don't need to specify the username, password
                 .permitAll()                                          //will go to the main page (index.html) with localhost:8080
 
-                .antMatchers("/api/**")               //only student role user can access,
+                .antMatchers("/api/**")               //only student role user can access localhost:8080/api/...,
                 .hasRole(ApplicationUserRole.STUDENT.name())    //Role Based authentication to protect API from ADMIN ROLE
 
                 //User Roles who has permission of COURSE_WRITE can access and DELETE,POST,and PUT the data
@@ -92,6 +91,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     * The @Bean annotation returns an object that spring registers as a bean in application context.
     * The logic inside the method is responsible for creating the instance.
     * */
+    /* replaced by ApplicationUserService
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
@@ -121,5 +121,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 lindaUser,
                 tomUser
         );
+    }*/
+
+    //configuring the daoAuthenticationProvider is the way to use the custom ApplicationUserService that implements the UserDetailsService
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
